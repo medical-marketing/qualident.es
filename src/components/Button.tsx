43196d3c8918915a42afbacceab782e46e5cd794
templar-ajay <templar.ajay@gmail.com>
2023-12-16 19:11:09 +0530

    button code refactor

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,33 @@ import clsx from "clsx";
 import { PrismicNextLink } from "@prismicio/next";
 import { getSettings } from "@/app/utils";
 
+type getButtonStylesProps = {
+  variant: "filled" | "bordered";
+  cta_text_color?: string;
+  cta_background_color?: string;
+};
+
+function getButtonStyles({
+  variant = "filled",
+  cta_text_color,
+  cta_background_color,
+}: getButtonStylesProps) {
+  if (variant == "filled") {
+    return {
+      color: cta_text_color || "rgb(250,255,255)",
+      backgroundColor: cta_background_color || "rgb(234,179,8)",
+    };
+  } else {
+    return {
+      color: cta_text_color || "#000",
+      backgroundColor: "transparent",
+      border: "4px solid",
+      borderRadius: "10px",
+      borderColor: cta_background_color || "#000",
+    };
+  }
+}
+
 export default async function Button({
   cta_link,
   iframe,
@@ -14,23 +41,18 @@ export default async function Button({
   const { cta_background_color, cta_text_color, default_iframe } =
     settings.data;
 
+  const theButtonStyles = getButtonStyles({
+    variant: "bordered",
+    ...{ cta_background_color, cta_text_color },
+  });
+
   return (
     <div className="relative flex justify-center">
       {iframe?.id ? (
         <CTA
           iframe={<Iframe iframe={iframe || default_iframe}></Iframe>}
           className={className}
-          // style={{
-          //   color: cta_text_color || "rgb(250,255,255)",
-          //   backgroundColor: cta_background_color || "rgb(234,179,8)",
-          // }}
-          style={{
-            color: cta_text_color || "#000",
-            backgroundColor: "transparent",
-            border: "4px solid",
-            borderRadius: "10px",
-            borderColor: cta_background_color || "#000",
-          }}
+          style={theButtonStyles}
         >
           {children}
         </CTA>
@@ -40,30 +62,12 @@ export default async function Button({
             "flex justify-center w-fit transition-colors duration-200 ease-in-out py-4 md:py-6 px-8 md:px-12 font-display font-semibold text-lg md:text-2xl text-center tracking-wide text-white bg-yellow-400 hover:bg-yellow-500",
             className
           )}
-          style={{
-            color: cta_text_color || "#000",
-            backgroundColor: "transparent",
-            border: "4px solid",
-            borderRadius: "10px",
-            borderColor: cta_background_color || "#000",
-          }}
+          style={theButtonStyles}
           field={cta_link}
         >
           {children}
         </PrismicNextLink>
       )}
-
-      {/* <PrismicNextLink
-        className={clsx(
-          "block w-full bg-yellow-400 hover:bg-yellow-500 transition-colors duration-200 ease-in-out py-4 md:py-6 px-8 md:px-12 font-display font-semibold text-lg md:text-2xl text-center text-white tracking-wide",
-          className
-        )}
-        style={{
-          color: cta_text_color || "rgb(250,255,255)",
-          backgroundColor: cta_background_color || "rgb(234,179,8)",
-        }}
-        {...restProps}
-      /> */}
     </div>
   );
 }
